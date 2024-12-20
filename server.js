d5f976e1fb7cf6011cedbf6763d9b3250d816604
mohd-momin <mohammadmomin_se21a14_53@dtu.ac.in>
2024-12-20 20:22:00 +0530

    commit

--- server.js
+++ server.js
@@ -7,18 +7,12 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
-import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-
-//ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./client/build")))
+
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -35,9 +29,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+app.get("/", (req, res) => {
+  res.send("<h1>auraperfumes</h1>");
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
